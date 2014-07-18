bdbd494872cbe46b0b799689b909c578348b90e2
Robert Attaway <rattaway2@student.gsu.edu>
2014-07-18 15:17:09 -0400

    Index and initial support files
    I created the index file for displaying the blog owners and titles. Then
    the rest of the files we can all just add to them as needed. We can get
    a nice look and feel by reviewing the base CSS classes I made and follow
    that motif moving forward

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <head>
+        <title>Bad Ass Blogs, Inc.</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link href="badass.css" type="text/css" rel="Stylesheet" media="screen" />
+    </head>
+    <body>
+    </body>
+</html>
