a2f916223010a300778133b0b890ea053341c1ad
CaptainAdams <evanadams28@gmail.com>
2014-07-22 21:17:10 -0400

    Add comments
    No fetching or editing functionality yet -- in progress now.

---
+++ comments.php
@@ -0,0 +1,25 @@
+<?php 
+    include_once "dataaccess.php";
+    include_once "sessionManager.php";
+?>
+<html>
+	<head>
+	</head>
+	<body>
+		<form name='contentForm' action="" method="post">
+			<textarea rows='10' cols='50' name='contentBox'></textarea>
+			<br><br>
+			<button type="submit">Add Comment</button>
+		</form>
+		
+		<?php
+			if(isset($_POST["contentBox"])){
+				$comContent = $_POST["contentBox"];
+				$userId = getUserId($_SESSION["username"]);
+				$postId = $_SESSION["selectedPostId"];
+				addComment($postId, $userId, $comContent);
+			}
+		?>
+		
+	</body>
+</html>
